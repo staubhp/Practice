a0fe5b6c77faba68da4b4b5a61bca9680dcd6130
Payton Staub <pstaub@pstaub-lt.cl.local>
2015-03-15 13:28:11 -0400

    Added strtok implementation

--- misc-practice/misc-practice/Program.cs
+++ misc-practice/misc-practice/Program.cs
@@ -10,7 +10,8 @@ namespace misc_practice
     {
         static void Main(string[] args)
         {
-            duplicateRemove();
+            //duplicateRemove();
+            stringTokenizer();
 
         }
 
@@ -25,6 +26,44 @@ namespace misc_practice
             Console.WriteLine("Output: " + string.Join(", ", removed.Select(x => x.ToString())));
             duplicateRemove();
 
+        }
+
+        static void stringTokenizer()
+        {
+            Console.WriteLine("String Tokenizer");
+            Console.WriteLine("Enter input string:");
+            string input = Console.ReadLine();
+            Console.WriteLine("Enter delimiter char:");
+            char delimiter = Console.ReadLine()[0];
+            var myTokenizer = new StringTokenizer();
+            var tokens = myTokenizer.strtok(delimiter, input);
+            Console.WriteLine("Output: " + string.Join(",", tokens.Select(x => x)));
+            stringTokenizer();
+                
+        }
+    }
+
+    class StringTokenizer
+    {
+        public string[] strtok(char delimiter, string input)
+        {
+            string currentToken ="";
+            List<int> delimiterIndices = new List<int>();
+            for (int i=0; i<input.Length; i++){
+                if (input[i] == delimiter)
+                    delimiterIndices.Add(i);
+            }
+
+            string[] ret = new string[delimiterIndices.Count + 1];
+            for (int i=0; i<delimiterIndices.Count; i++){
+                ret[i] = input.Substring((i == 0 ? 0 : delimiterIndices[i-1]+1), delimiterIndices[i]-(i == 0 ? 0 : delimiterIndices[i-1]+1));
+                if (i == delimiterIndices.Count - 1)
+                {
+                    ret[i + 1] = input.Substring(delimiterIndices[i]+1);
+                }
+            }
+            
+            return ret;
         }
     }
 
@@ -59,3 +98,4 @@ namespace misc_practice
         }
     }
 }
+ 
